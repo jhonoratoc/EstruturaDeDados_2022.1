c6f67da556053206512908d1d60f902519b00d52
João Honorato <joao.honorato@academico.ufpb.br>
2022-09-18 22:02:11 -0300

    corrige nome das funções

--- exercicios_aula/ex_listacircular.c
+++ exercicios_aula/ex_listacircular.c
@@ -8,45 +8,101 @@ struct lista{
     int vetor[N];
 };
 
-void criar_lista(struct lista *);
+
+int menu(struct lista *l);
+void cria_lista(struct lista *);
 
 int esta_cheia(struct lista *);
 int esta_vazia(struct lista *);
 
-void inserir_fim(struct lista *);
-void inserir_inicio(struct lista *);
+void insere_fim(struct lista *);
+void insere_inicio(struct lista *);
 
-void limpar_lista(struct lista *);
-void mostrar_lista(struct lista *);
+void limpa_lista(struct lista *);
+void mostra_lista(struct lista *);
 
-void remover_fim(struct lista *);
-void remover_inicio(struct lista *);
+void remove_fim(struct lista *);
+void remove_inicio(struct lista *);
+
 
 
 int main(){
 
     struct lista l1;
-    criar_lista(&l1);
-    inserir_fim(&l1);
-    inserir_fim(&l1);
-    //inserir_inicio(&l1);
-    mostrar_lista(&l1);
-    limpar_lista(&l1);
-    inserir_fim(&l1);
-    inserir_fim(&l1);
-    mostrar_lista(&l1);
+    cria_lista(&l1);
+    menu(&l1);
 
     return 0;
 }
 
 
 
+int menu(struct lista *l){
+    puts("\nDigite o que deseja fazer:");
+    puts("1: mostrar lista");
+    puts("2: inserir elemento na lista");
+    puts("3: remover elemento na lista");
+    puts("4: limpar a lista");
+    puts("0: encerrar");
 
+    int escolha;
+    scanf("%d", &escolha);
 
-void criar_lista(struct lista *l){
+    switch (escolha) {
+        case 1: mostra_lista(l);
+            break;
+        case 2: 
+            puts("1: inserir no inicio");
+            puts("2: inserir no fim");
+            scanf("%d", &escolha);
+
+            switch (escolha){
+                case 1: insere_inicio(l);
+                    break;
+                case 2: insere_fim(l);
+                    break;
+                default:{
+                    puts("Codigo invalido!");
+                    break;}
+            }
+
+            break;
+        case 3:
+            puts("1: remover no inicio");
+            puts("2: remover no fim");
+            scanf("%d", &escolha);
+
+            switch (escolha){
+                case 1: remove_inicio(l);
+                    break;
+                case 2: remove_fim(l);
+                    break;
+                default:{
+                    puts("Codigo invalido!");
+                    break;}
+            }
+
+            break;
+        case 4: limpa_lista(l);
+            break;
+        case 0:
+            return 0;
+        default:
+            puts("Codigo invalido!");
+            break;
+    }
+
+    menu(l);
+    
+}
+
+void cria_lista(struct lista *l){
     l->cti=-1;
     l->ctf=-1;
 }
+
+
+
 
 int esta_cheia(struct lista *l){
     int cti_temp;
@@ -70,8 +126,8 @@ int esta_vazia(struct lista *l){
 
 
 
-void inserir_fim(struct lista *l){
-    if (esta_cheia(l) == 1)
+void insere_fim(struct lista *l){
+    if (esta_cheia(l))
         puts("Lista cheia!");
 
     else{ 
@@ -81,7 +137,7 @@ void inserir_fim(struct lista *l){
 
         if (l->ctf == N-1)
             l->ctf = 0;
-        else if (l->cti == -1){
+        else if (esta_vazia(l)){
             l->cti = 0;
             l->ctf = 0;
         }else 
@@ -91,8 +147,8 @@ void inserir_fim(struct lista *l){
     }
 }
 
-void inserir_inicio(struct lista *l){
-    if (esta_cheia(l) == 1)
+void insere_inicio(struct lista *l){
+    if (esta_cheia(l))
         puts("Lista cheia!");
 
     else{
@@ -102,7 +158,7 @@ void inserir_inicio(struct lista *l){
 
         if (l->cti == 0)
             l->cti == N-1;
-        else if (l->cti == -1){
+        else if (esta_vazia(l)){
             l->cti = 0;
             l->ctf = 0;
         }else
@@ -116,29 +172,34 @@ void inserir_inicio(struct lista *l){
 
 
 
-void limpar_lista(struct lista *l){
-    l->cti = l->ctf = -1;
+void limpa_lista(struct lista *l){
+    l->cti=-1;
+    l->ctf=-1;
 }
 
-void mostrar_lista(struct lista *l){
+void mostra_lista(struct lista *l){
     int i = l->cti;
-    while(i!=(l->ctf)+1){
-        printf("%d ", l->vetor[i]);
+    for (; i != l->ctf;){
+        printf(" %d", l->vetor[i]);
         if (i == N-1)
             i = 0;
         else
             i++;
     }
+    printf(" %d", l->vetor[l->ctf]);
 }
 
 
 
 
 
-void remover_fim(struct lista *l){
+
+
+
+void remove_fim(struct lista *l){
 
 }
 
-void remover_inicio(struct lista *l){
+void remove_inicio(struct lista *l){
 
 }
