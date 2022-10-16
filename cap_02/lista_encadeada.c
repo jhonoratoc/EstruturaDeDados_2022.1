d8fc50c03e68ab79e38f15c790e01ff88a630a24
João Honorato <joao.honorato@academico.ufpb.br>
2022-10-15 22:20:17 -0300

    funcoes de insere e remove fim

--- cap_02/lista_encadeada.c
+++ cap_02/lista_encadeada.c
@@ -12,6 +12,9 @@ Lista* cria_lista();
 void insere_inicio(Lista **ll, int n);
 void remove_inicio(Lista **ll);
 
+void insere_fim(Lista *l, int n);
+void remove_fim(Lista *l);
+
 int lista_vazia(Lista *l);
 
 
@@ -30,17 +33,37 @@ Lista *cria_lista(){
 }
 
 void insere_inicio(Lista **ll, int n){
-    Lista *novo = (Lista*) malloc(sizeof(Lista));
-    novo->valor = n;
-    novo->prox = *ll;
-    *ll = novo;
+    Lista *novo = (Lista*) malloc(sizeof(Lista));       // alocando memória para o novo elemento
+    novo->valor = n;                                    // preenchendo o novo elemento
+    novo->prox = *ll;                                   // encadeando o novo elemento ao antigo primeiro elemento da lista
+    *ll = novo;                                         // adicionando o elemento ao início da lista
 }
 
 void remove_inicio(Lista **ll){
-    Lista *temp = *ll;
-    *ll = (*ll)->prox;
-    free(temp);
+    Lista *temp = *ll;                                  // criando um ponteiro para lista para não perder o acesso ao elemento a ser retirado
+    *ll = (*ll)->prox;                                  // a partir de agora, a lista começará do antigo segundo elemento
+    free(temp);                                         // liberando o elemento retirado, utilizando o ponteiro para lista
 }
+
+
+
+void insere_fim(Lista *l, int n){
+    Lista *novo = (Lista*) malloc(sizeof(Lista));       // alocando memória para o novo elemento
+    novo->valor = n;
+    Lista *no = l;
+    for (; no->prox != NULL; no = no->prox);
+    no->prox = novo;
+    novo->prox = NULL;
+}
+
+void remove_fim(Lista *l){
+    Lista *temp = l;                                        // criando um ponteiro para lista para acessar o último elemento
+    for (; temp->prox->prox != NULL; temp = temp->prox);    // acessando o PENÚLTIMO elemento
+    free(temp->prox);                                       // liberando o último elemento
+    temp->prox = NULL;                                      // transformando o penúltimo elemento no novo último elemento
+}
+
+
 
 int lista_vazia(Lista *l){
     return (l == NULL);         // retorna 1 se a lista estiver vazia
