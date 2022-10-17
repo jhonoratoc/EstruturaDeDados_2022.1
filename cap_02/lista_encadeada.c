0537c497d16279cd27c0f5bf59371d8902208f61
João Honorato <joao.honorato@academico.ufpb.br>
2022-10-16 21:16:59 -0300

    adiciona menu

--- cap_02/lista_encadeada.c
+++ cap_02/lista_encadeada.c
@@ -17,15 +17,17 @@ void remove_fim(Lista **ll);
 
 void mostra_lista(Lista **ll);
 
-void limpa_lista(Lista **ll);
-
+void limpa_lista(Lista **ll);               // precisa de ponteiro pra ponteiro
 int lista_vazia(Lista *l);
+
+int menu(Lista **ll);
 
 
 
 int main(){
 
     Lista *lista = cria_lista();
+    menu(&lista);
 
     return 0;
 
@@ -54,12 +56,16 @@ void remove_inicio(Lista **ll){
 
 
 void insere_fim(Lista **ll, int n){
-    Lista *novo = (Lista*) malloc(sizeof(Lista));       // alocando memória para o novo elemento
-    novo->valor = n;                                    // construindo o novo elemento
-    Lista *no = *ll;                                    // Lista *no com o objetivo de percorrer a lista
-    for (; no->prox != NULL; no = no->prox);            // no = atual último elemento da lista
-    no->prox = novo;                                    // adiciona o novo elemento ao final da lista
-    novo->prox = NULL;                                  // o novo último elemento aponta para NULL
+    if (*ll == NULL)
+        insere_inicio(ll, n);
+    else{
+        Lista *novo = (Lista*) malloc(sizeof(Lista));       // alocando memória para o novo elemento
+        novo->valor = n;                                    // construindo o novo elemento
+        Lista *no = *ll;                                    // Lista *no com o objetivo de percorrer a lista
+        for (; no->prox != NULL; no = no->prox);            // no = atual último elemento da lista
+        no->prox = novo;                                    // adiciona o novo elemento ao final da lista
+        novo->prox = NULL;                                  // o novo último elemento aponta para NULL
+    }
 }
 
 void remove_fim(Lista **ll){
@@ -71,7 +77,10 @@ void remove_fim(Lista **ll){
 
 
 void mostra_lista(Lista **ll){
-    
+    Lista *no = *ll;
+    for (; no != NULL; no = no->prox)
+        printf("%d ", no->valor);
+    puts("");
 }
 
 
@@ -81,7 +90,7 @@ void limpa_lista(Lista **ll){
     *ll = NULL;                                 // esvazia a lista
 
     // hora de liberar os elementos
-    for (; temp_prox =! NULL; temp_prox = temp_prox->prox){
+    for (; temp_prox != NULL; temp_prox = temp_prox->prox){
         temp_free->prox = NULL;         // anula o ponteiro pro próximo elemento
         free(temp_free);                // libera o elemento
         temp_free = temp_prox;          // prepara o próximo elemento a ser liberado
@@ -91,7 +100,59 @@ void limpa_lista(Lista **ll){
     free(temp_free);
 }
 
-
 int lista_vazia(Lista *l){
     return (l == NULL);         // retorna 1 se a lista estiver vazia
+}
+
+
+int menu(Lista **ll){
+    puts("Digite o que deseja fazer:");
+    puts("1: mostrar a lista");
+    puts("2: inserir elemento no início da lista");
+    puts("3: inserir elemento no fim da lista");
+    puts("4: remover elemento no início da lista");
+    puts("5: remover elemento no fim da lista");
+    puts("6: limpar a lista");
+    puts("-1: encerrar");
+
+    int escolha;
+    scanf("%d", &escolha);
+    int n;
+
+    switch (escolha) {
+        case 1: puts("\nSua lista: ");
+                mostra_lista(ll);
+                break;
+
+        case 2: puts("Que elemento deseja inserir? ");
+                scanf("%d", &n);
+                insere_inicio(ll, n);
+                puts("inserido!");
+                break;
+
+        case 3: puts("Que elemento deseja inserir? ");
+                scanf("%d", &n);
+                insere_fim(ll, n);
+                puts("inserido!");
+                break;
+
+        case 4: remove_inicio(ll);
+                puts("removido!");
+                break;        
+        
+        case 5: remove_fim(ll);
+                puts("removido!");
+                break;
+
+        case 6: limpa_lista(ll);
+                puts("lista limpa!");
+                break;
+
+        case -1: return 0;
+
+        default: puts("Valor invalido!");
+                break;
+        }
+
+    menu(ll);
 }
