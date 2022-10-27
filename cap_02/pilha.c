b07b24d710c11f89196bfe36e9df7592eb13cdd1
João Honorato <joao.honorato@academico.ufpb.br>
2022-10-27 15:52:58 -0300

    implementação de pilha

--- cap_02/pilha.c
+++ cap_02/pilha.c
@@ -12,7 +12,7 @@ Pilha *cria_pilha();
 
 void limpa_pilha(Pilha **pp);
 
-void mostra_pilha(Pilha **pp);
+void mostra_pilha(Pilha *p);
 void mostra_topo(Pilha **pp);
 
 void pop(Pilha **pp);
@@ -34,9 +34,22 @@ Pilha *cria_pilha(){
     return NULL;
 }
 
-void limpa_pilha(Pilha **pp);
+void limpa_pilha(Pilha **pp){
+    if(*pp){
+        Pilha *aux;
+        aux = (*pp)->prox;
+        free(*pp);
+        *pp = NULL;
+        limpa_pilha(&aux);
+    } 
+}
 
-void mostra_pilha(Pilha **pp);
+void mostra_pilha(Pilha *p){
+    if(!p) return;
+
+    printf("%d ", p->valor);
+    mostra_pilha(p->prox);
+}
 
 void mostra_topo(Pilha **pp){
     if(!(*pp)){
@@ -44,7 +57,7 @@ void mostra_topo(Pilha **pp){
         return;
     }
 
-    printf("Topo da pilha:\n%d", (*pp)->valor);
+    printf("\nTopo da pilha:\n%d\n\n", (*pp)->valor);
 }
 
 void pop(Pilha **pp){
@@ -72,10 +85,11 @@ void push(Pilha **pp, int n){
 
 void menu(Pilha **pp){
     puts("Digite o que deseja fazer:");
-    puts("1: mostrar o topo da pilha");
-    puts("2: inserir elemento (push)");
-    puts("3: remover elemento (pop)");
-    puts("4: limpar a lista");
+    puts("1: mostrar a pilha");
+    puts("2: mostrar o topo da pilha");
+    puts("3: inserir elemento (push)");
+    puts("4: remover elemento (pop)");
+    puts("5: limpar a lista");
     puts("-1: encerrar");
 
     int escolha;
@@ -83,19 +97,32 @@ void menu(Pilha **pp){
     int n;
 
     switch (escolha) {
-        case 1: puts("\nTopo da pilha: ");
-                mostra_topo(pp);
+        case 1: puts("\nPilha: ");
+                if(!(*pp)){
+                    puts("\n(pilha vazia)\n");
+                    break;
+                }
+                mostra_pilha(*pp);
+                puts("\n");
                 break;
 
-        case 2: puts("\nQue elemento deseja inserir? ");
+        case 2: mostra_topo(pp);
+                break;
+
+        case 3: puts("\nQue elemento deseja inserir? ");
                 scanf("%d", &n);
                 push(pp, n);
                 break;
 
-        case 3: pop(pp);
+        case 4: pop(pp);
                 break;
 
-        case 4: limpa_pilha(pp);
+        case 5: limpa_pilha(pp);
+                if(!(*pp)){
+                    puts("\npilha vazia!\n");
+                    break;
+                }
+                puts("\npilha limpa!\n");
                 break;
 
         case -1: return;
